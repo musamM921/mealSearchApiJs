c752db0752572e153cab41b5ac278aedddc0d778
musamM921 <musaartlove@gmail.com>
2024-05-09 20:16:42 +0400

    api update

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ searchBtn.addEventListener('click', getMealList)
 
 function getMealList() {
     let searchInputTxt = document.getElementById
-    ('search-input').value.trim()
-    
+        ('search-input').value.trim()
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+        .then(response => response.json())
+        .then(data => {
+            let html = ""
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                <div class = "meal-item" data-id = "${meal.idMeal}">
+                <div class = "meal-img">
+                  <img src = "${meal.strMealThumb}"
+                   alt = "food">
+                </div>
+                <div class = "meal-name">
+                  <h3>${meal.strMeal}</h3>
+                  <a href = "#" class = "recipe-btn">Get Recipe</a>
+                </div>
+              </div>
+                `;
+                })
+            }
+            mealList.innerHTML = html;
+        })
 }
