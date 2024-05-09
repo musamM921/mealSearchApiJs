ec4d842ba5886ef41f34d25039eeae323533c180
musamM921 <musaartlove@gmail.com>
2024-05-09 19:57:31 +0400

    input update

---
+++ index.js
@@ -0,0 +1,12 @@
+const searchBtn = document.getElementById('search-btn')
+const mealList = document.getElementById('meal')
+const mealDetailsContent = document.querySelector('.meal-details-content')
+const recipeCloseBtn = document.getElementById('recipe-close-btn')
+
+searchBtn.addEventListener('click', getMealList)
+
+function getMealList() {
+    let searchInputTxt = document.getElementById
+    ('search-input').value.trim()
+    
+}
